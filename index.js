1622ee93876f284c57d9c074c7e2bf79b8cf3e17
nightmarelullaby <83048231+nightmarelullaby@users.noreply.github.com>
2025-09-12 22:29:58 -0400

    feat: add yt-dlp as executable

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import express from 'express';
-import ytdl from 'ytdl-core';
-import exec from 'yt-dlp-exec';
+// import exec from 'yt-dlp-exec';
 import cors from 'cors';
+import { execFile } from 'child_process';
+import path from 'path';
 
 const app = express();
 const port = 3000;
@@ -12,14 +13,20 @@ app.get('/api/download', async (req, res) => {
   const { url } = req.query;
   if(!url) return res.status(400).json({ error: 'invalid url' });
 
-  try {
-      const result = await exec(url, {
-          dumpSingleJson: true,
-          noWarnings: true,
-          noCallHome: true,
-          noCheckCertificate: true
-      });
 
+execFile("./yt-dlp", [
+  url,
+  '--dump-single-json',
+  '--no-warnings',
+  '--no-call-home',
+  '--no-check-certificate'
+], (error, stdout, stderr) => {
+  if (error) {
+    console.error('yt-dlp error:', error);
+    return;
+  }
+  try {
+        const result = JSON.parse(stdout);
         const formats = result.formats.map(f => ({
             format_id: f.format_id,
             resolution: f.height ? `${f.height}p` : 'audio',
@@ -37,6 +44,8 @@ app.get('/api/download', async (req, res) => {
     return res.status(500).json({ error: 'Failed to fetch video info' });
   }
 });
+
+})
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
