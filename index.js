38dfcf8c21238f4f8d7ee24a85d097a68a762abd
nightmarelullaby <83048231+nightmarelullaby@users.noreply.github.com>
2025-11-23 23:59:23 -0400

    fixes

--- index.js
+++ index.js
@@ -16,27 +16,25 @@ app.get('/api/download', async (req, res) => {
   if (!url || typeof url !== 'string' || !url.startsWith('http')) {
     return res.status(400).json({ error: 'Invalid YouTube URL' });
   }
-
+  const ls = runYtDlp(url, ['--print-json', '-q', '--skip-download']);
+  console.log('here!')
   try {
-    const ls = runYtDlp(url, ['--print-json', '-q', '--skip-download']);
     let buffer = '';
     ls.stdout.on('data', async (data) => {
         buffer += data.toString();
-       console.log('retreiveng data...')
-        // Split into lines
+
         let lines = buffer.split('\n');
         buffer = lines.pop(); // keep incomplete line
 
         for (const line of lines) {
             if (line.trim()) {
-            try {
-                const obj = JSON.parse(line);
-                return res.json(obj);
+              try {
+                  const obj = JSON.parse(line);
+                  return res.json(obj);
 
-            } catch (err) {
-                console.error('Invalid JSON line:', line);
-                reject(err);
-            }
+              } catch (err) {
+                  console.error('Invalid JSON line:', line);
+              }
             }
         }
     })
